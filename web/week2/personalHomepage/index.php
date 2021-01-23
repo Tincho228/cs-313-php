76a730258bbfe9cd4e162d3f4f96c1f0e653f35b
Tincho228 <martin_quintero521@hotmail.com>
2021-01-23 12:25:37 -0300

    new

--- web/week2/personalHomepage/index.php
+++ web/week2/personalHomepage/index.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="week2/css/week2-styles.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="js/jquery.js"></script>
@@ -26,7 +26,7 @@
         </header>
         <main>
             <div class="hero_div">
-                <img class="hero_image" src="images/computer.png" alt="hero image">
+                <img class="hero_image" src="images/computer.jpg" alt="hero image">
                 <div class="user_div">
                     <div class="user_box">
                         <img class="user_image" src="images/user.png" alt="user picture">
@@ -111,7 +111,6 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
 </body>
 
 </html>
