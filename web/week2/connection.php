67436e339907f9cea51825f57af9813bbad8a767
Tincho228 <martin_quintero521@hotmail.com>
2021-02-01 14:15:10 -0300

    git

--- web/week2/connection.php
+++ web/week2/connection.php
@@ -1,9 +1,29 @@
 <?php
 function connection(){
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
   
-  $db = parse_url(getenv("DATABASE_URL"));
-  echo $db;
-
+    $dbOpts = parse_url($dbUrl);
+  
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+  
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    echo "conexion";
+  
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+   
+ 
 }
 connection();
 ?>
