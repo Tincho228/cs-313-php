24068a248ea6ccaec9d580702f61e1fc929f08a8
Tincho228 <martin_quintero521@hotmail.com>
2021-02-06 14:13:41 -0300

    t

--- web/Personal-Project/index.php
+++ web/Personal-Project/index.php
@@ -10,12 +10,12 @@
 session_start();
 //$_SESSION['loggedin'] = 0;
 
-// Get the database connection file
+/*// Get the database connection file
 require_once 'library/connections.php';
 // Get the PHP Motors model for use as needed
 require_once 'model/main-model.php';
 // Get the functions library
-require_once 'library/functions.php';
+require_once 'library/functions.php';*/
 
 
 $action = filter_input(INPUT_POST, 'action');
