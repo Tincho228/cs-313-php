ef8fe5caa988b404c4f0d8979008c83c3c633459
Tincho228 <martin_quintero521@hotmail.com>
2021-02-06 14:05:00 -0300

    t

---
+++ web/Personal-Project/index.php
@@ -0,0 +1,36 @@
+<?php
+
+/*********************************
+  
+    THIS IS THE MAIN CONTROLLER
+
+**********************************/
+
+// Create or access a Session
+session_start();
+//$_SESSION['loggedin'] = 0;
+
+// Get the database connection file
+require_once 'library/connections.php';
+// Get the PHP Motors model for use as needed
+require_once 'model/main-model.php';
+// Get the functions library
+require_once 'library/functions.php';
+
+
+$action = filter_input(INPUT_POST, 'action');
+ if ($action == NULL){
+  $action = filter_input(INPUT_GET, 'action');
+ }
+
+// Check if the firstname cookie exists, get its value
+
+switch ($action){
+    case 'template':
+     include 'view/template.php';
+    break;
+     
+    default:
+     include 'views/home.php';
+ }
+ ?>
