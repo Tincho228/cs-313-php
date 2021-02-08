b51b80806110a465114831cf7a26f738097b0b67
Tincho228 <martin_quintero521@hotmail.com>
2021-02-08 15:42:40 -0300

    r

--- web/Personal-Project/views/login.php
+++ web/Personal-Project/views/login.php
@@ -20,7 +20,6 @@
     //tag to show any messages that may need to be displayed
     if (isset($_SESSION['message'])) {
     echo $_SESSION['message'];
-    echo $_SESSION['message_a'];
     } ?>
     <form action="../accounts/index.php" method="post" class="login-form">
         <h1 class="text-center text-light">Log In</h1>
