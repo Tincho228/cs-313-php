d4120e2a02c4b94993b1f56efcaf772d1074e7ce
Tincho228 <martin_quintero521@hotmail.com>
2021-02-08 16:37:47 -0300

    r

--- web/Personal-Project/model/accounts-model.php
+++ web/Personal-Project/model/accounts-model.php
@@ -7,15 +7,8 @@ function regClient($cl_firstname, $cl_lastname, $cl_email, $cl_password, $cl_pho
 //function regClient($cl_firstname){    
     
     
-    $db = herokuConnection();/*
-    $sql = 'SELECT * FROM public.clients WHERE cl_firstname = :cl_firstname';
-    $stmt = $db->prepare($sql);
-    $stmt->bindValue(':cl_firstname', $cl_firstname, PDO::PARAM_STR);
-    $stmt->execute();
-    $info = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $db = herokuConnection();
     
-    $stmt->closeCursor();
-    return $info;*/
     
     $sql = 'INSERT INTO public.clients (cl_firstname, cl_lastname, cl_email, cl_password, cl_phone)
         VALUES (:cl_firstname, :cl_lastname, :cl_email, :cl_password, :cl_phone)';
@@ -26,7 +19,7 @@ function regClient($cl_firstname, $cl_lastname, $cl_email, $cl_password, $cl_pho
     $stmt->bindValue(':cl_lastname', $cl_lastname, PDO::PARAM_STR);
     $stmt->bindValue(':cl_email', $cl_email, PDO::PARAM_STR);
     $stmt->bindValue(':cl_password', $cl_password, PDO::PARAM_STR);
-    $stmt->bindValue(':cl_phone', $cl_phone, PDO::PARAM_INT);
+    $stmt->bindValue(':cl_phone', $cl_phone, PDO::PARAM_STR);
     // Insert the data
     $stmt->execute();
     // Ask how many rows changed as a result of our insert
