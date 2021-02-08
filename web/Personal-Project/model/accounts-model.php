d6560cc1af7d287f4e5944bc29f8e41258f53342
Tincho228 <martin_quintero521@hotmail.com>
2021-02-08 15:23:08 -0300

    t

--- web/Personal-Project/model/accounts-model.php
+++ web/Personal-Project/model/accounts-model.php
@@ -1,7 +1,26 @@
 <?php
 //this is the accounts model.
 
-
+function herokuConnection(){
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+      $dbOpts = parse_url($dbUrl);
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $db;
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+  }
 //This function will handle site registrations.
 
 function regClient($cl_firstname, $cl_lastname, $cl_email, $cl_password, $cl_phone){
