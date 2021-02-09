2b451bcf8dda1a201e88e7c7789d15f5eb04ba5d
Tincho228 <martin_quintero521@hotmail.com>
2021-02-09 16:16:06 -0300

    e

--- web/Personal-Project/accounts/index.php
+++ web/Personal-Project/accounts/index.php
@@ -102,14 +102,14 @@ switch ($action){
                 exit;
             }
             else {
-                echo $cl_password;
-                echo $clientData['cl_password'];
                 $hashCheck = password_verify($cl_password, $clientData['cl_password']);
             }
         // If the hashes don't match create an error
         // and return to the login view
         if (!$hashCheck) {
                 $_SESSION['message'] = '<p>Please check your password and try again.</p>';
+                echo $cl_password;
+                echo $clientData['cl_password'];
                 include '../views/login.php';
                 exit;
         }else {
